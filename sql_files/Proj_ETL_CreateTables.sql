700aac4201da9525ff0b9fea87049bb60159968a
Bivittatus <Python.Bivittatus.8518@gmail.com>
2019-09-07 14:12:35 -0500

    9/7/2019 daniel updates

--- sql_files/Proj_ETL_CreateTables.sql
+++ sql_files/Proj_ETL_CreateTables.sql
@@ -3,23 +3,23 @@ DROP TABLE IF EXISTS mental_health_pol;
 
 
 CREATE TABLE neuroscience (
-  title character varying(250) NOT NUll,
-  citation_author character varying(350) NOT NUll,
-  citation_date integer NOT NULL,
-  cit_jrnl_title character varying(100) NOT NULL,
-  description character varying(3000) NOT NULL,
-  keywords character varying(100) NOT NULL
+  title character varying(250),
+  citation_author character varying(350),
+  citation_date integer,
+  citation_journal_title character varying(100),
+  description character varying(3000),
+  keywords character varying(1000)
 );
 
 
 CREATE TABLE mental_health_pol (
-  country character varying(100) NOT NUll,
-  year integer NOT NULL,
-  govt_exp_mntlhos_per_totgovtexp_mntl_hlth integer NOT NUll,
-  std_alone_law_mntl_hlth character varying(3) NOT NULL,
-  yr_law_enacted_latver integer NOT NULL,
-  std_alone_polplan_mnth_hlth character varying(3) NOT NULL,
-  pub_yr_polplan_latver integer NOT NULL
+  country character varying(100),
+  year integer,
+  govt_exp_mntlhos_per_totgovtexp_mntl_hlth float,
+  std_alone_law_mntl_hlth character varying(3),
+  yr_law_enacted_latver float,
+  std_alone_polplan_mnth_hlth character varying(3),
+  pub_yr_polplan_latver float
 );
 
 
